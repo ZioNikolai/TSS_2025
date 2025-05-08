9e681275ca4b86ffebc05b6b4badf2ba2e66a009
DarioIEL <dario.mennillo@immaginazioneelavoro.it>
2025-05-08 14:20:18 +0200

    upLoad Lezioni

--- Prj_1_MenuGiochi/src/giochini/Menu.java
+++ Prj_1_MenuGiochi/src/giochini/Menu.java
@@ -3,6 +3,21 @@ package giochini;
 import java.util.Scanner;
 
 public class Menu {
+	
+	public static void multipli() {
+		for(int i = 1; i <= 100; i++) {
+			if(i % 3 == 0 && i % 5 == 0) {
+				System.out.println(i + " BANGARANG");
+			}else if(i % 3 == 0) {
+				System.out.println(i + " BOOM");
+			}else if(i % 5 == 0) {
+				System.out.println(i + " ZOOM");
+			}else {
+				System.out.println(i);
+			}
+		}
+	}
+	
 	public static void main(String[] args) {
 
 		Scanner mioScann = new Scanner(System.in);
@@ -23,27 +38,39 @@ public class Menu {
 			case 'a':
 			case 'A':
 			case '1':
-				System.out.println("Hai scelto il gioco 1");
+				System.out.println("Hai scelto il gioco 1 dei multipli");
 				//multipli
 				//Stampa i numeri da 1 a 100. Per tutti i multipli di 3 stampa BOOM. Per tutti i multipli di 5 stampa ZOOM. Per tutti i multipli di 3 e 5 stampa BANGARANG
+				
+				//Modo 1: posso scrivere qui dentro tutta la logica
+				//Modo 2: posso creare un metodo ausiliare in questa classe
+				multipli();
+				//Modo 3: posso creare una classe a parte con dei metodi e richiamarli
+				
+				
 				break;
 			case '2':
 				System.out.println("Hai scelto il gioco 2");
 				//pattern
 				//Elabora un algoritmo per stampare il seguente Star Pattern
 				//
-				// @
-				// @@
-				// @@@
-				// @@@@
-				// @@@@@
+				//				Es4
+				//			     *
+				//			    * *
+				//			   *   *
+				//			  *     *
+				//			 *********  
+				Triangolo.disegnaTriangolo();
 
 				break;
 			case '3':
-				System.out.println("Hai scelto il gioco 3");
+				System.out.println("Hai scelto il gioco 3 - Il gioco della tombola");
 				//tombola
 				//Ricrea il gioco della tombola. Da un set di 90 numeri estraine 20. Mi raccomando non posso estrarre 2 volte lo stesso numero.
-			
+				Scanner input = new Scanner(System.in);
+				System.out.println("Quanti numeri vuoi estrarre ? ");
+				Tombola.estrazione3(input.nextInt());
+				
 				break;
 			case 'q':
 				System.out.println("Stai uscendo dal programma...");
